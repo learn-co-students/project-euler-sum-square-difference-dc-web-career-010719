04b525afa2f3fbe42f833c9ae795567353749ae1
Ben Yellin <byellindc@gmail.com>
2019-04-15 09:45:31 -0400

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,18 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum_squares
+    (1..@limit).reduce(0) {|memo, n| memo + (n**2)}
+  end
+
+  def square_sum
+    (1..@limit).reduce(&:+)**2
+  end
+
+  def difference
+    square_sum(@limit) - sum_squares(@limit)
+  end
+end
